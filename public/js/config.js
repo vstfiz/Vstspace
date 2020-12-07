bdc2200c7c58140683dad146f72f98e81ba874b3
vstfiz <vivek.sharma7354@gmail.com>
2020-12-07 21:12:04 +0530

    project completed

---
+++ public/js/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDhwjLN_Cdl0M28Ho9eAlulHrocuhAC7HY",
+    authDomain: "vstspace-2e24b.firebaseapp.com",
+    projectId: "vstspace-2e24b",
+    storageBucket: "vstspace-2e24b.appspot.com",
+    messagingSenderId: "913273417898",
+    appId: "1:913273417898:web:b4c80d823e6bd4275820d3",
+    measurementId: "G-DVT6LB4NX1"
+};
+firebase.initializeApp(firebaseConfig);
